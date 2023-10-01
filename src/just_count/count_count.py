68b25ebafcb61d6dea57add159c88777e3361e17
Olivier Swaak <o.swaak@hotmail.com>
2023-10-01 22:30:29 +0200

    feat: added click functionality

--- src/just_count/count_count.py
+++ src/just_count/count_count.py
@@ -1,7 +1,26 @@
 from just_count import square
+import click
 
-def main():
-    print(f"The square of 5 is {square.square(5)}")
+@click.command()
+@click.option(
+    "-n",
+    "--number",
+    default=5,
+    type = float,
+    help="Number to square.",
+    show_default=True,  # show default in help
+)
+@click.option(
+    "-sr",
+    "--square_root/--square",
+    is_flag = True,
+    help = "Calculate the square root instead"
+)
+def main(number, square_root):
+    if square_root:
+        print(f"The square root of {number} is {square.square_root(number)}")
+    else:
+        print(f"The square of {number} is {square.square(number)}")
 
 if __name__ == '__main__':
     main()
--- src/just_count/square.py
+++ src/just_count/square.py
@@ -1,6 +1,8 @@
 def square(x):
     return x**2
 
+def square_root(x):
+    return x ** 0.5
 
 if __name__ == "__main__":
     print(f"The square of 4 is {square(4)}")
